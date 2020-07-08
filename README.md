ad9d162e4bfce2f61d77ea2270c55c305765a9d8
Sam Keathley <samncollette@gmail.com>
2020-07-08 15:04:26 -0700

    fix date and battery life update issue

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Fitbit Versa D20 Clockface   
 
-This is a simple custom digital watchface for the Fitbit Versa family. This features  a simple D20 background that I made with SVG and only displays the date, time, and battery percentage. All of the art is original.
+This is a simple custom digital watchface for the Fitbit Versa family. This features a simple D20 background that I made with SVG and only displays the date, time, and battery percentage. All of the art is original.
 
 I build this watchface because I wanted something that was more "me" and I also don't care much about the health statistic aspects of the watch.
+
+### Technologies used
+---
+- FitBit SDK / Clock API
+- Tested functionality with FitBit Studio & virtual OS Simulator
 
 ![Screenshot](resources/D20Versa.png?raw=true "Screenshot of the watchface")
--- app/index.js
+++ app/index.js
@@ -4,42 +4,51 @@ import { battery } from "power";
 import { preferences } from "user-settings";
 import * as util from "../common/utils";
 
-// Update the clock every minute
-clock.granularity = "minutes";
-
-// Get a handle on the <text> element
 const myHour = document.getElementById("myHour");
 const myMin = document.getElementById("myMin");
 const myDate = document.getElementById("myDate");
 const myDayName = document.getElementById("myDayName");
 const myBattery = document.getElementById("myBattery");
 
-// Update the <text> element every tick with the current time
+let time = document.getElementById("time")
+
+clock.granularity = "minutes";
 clock.ontick = (evt) => {
   let today = evt.date;
   let hours = today.getHours();
   if (preferences.clockDisplay === "12h") {
     // 12h format
-    hours = ("0" + hours % 12 || 12).slice(-2);
+    hours = ("0" + hours % 12 || 12);
   } else {
     // 24h format
     hours = util.zeroPad(hours);
   }
   let mins = util.zeroPad(today.getMinutes());
-   myHour.text = `${hours}`;
-   myMin.text = `${mins}`;
+  myHour.text = `${hours}`;
+  myMin.text = `${mins}`;
 }
 
-let batteryLife = Math.floor(battery.chargeLevel) + "%";
+function updateBattery() {
+  let batteryLife = Math.floor(battery.chargeLevel) + "%";
+  myBattery.text = `${batteryLife}`;
+}
 
-let months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
-let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+updateBattery();
+setInterval(updateBattery, 10000);
 
-let today = new Date();
-let month = months[today.getMonth()];
-let dayName = days[today.getDay()];
-let date = ("0" + today.getDate()).slice(-2);
+function updateDate() {
+  let months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+  let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
-myDate.text = `${date} ${month}`;
-myDayName.text = `${dayName}`;
-myBattery.text = `${batteryLife}`;
+  let today = new Date();
+  let month = months[today.getMonth()];
+  let dayName = days[today.getDay()];
+  let date = ("0" + today.getDate()).slice(-2);
+  myDate.text = `${date} ${month}`;
+  myDayName.text = `${dayName}`;
+}
+
+updateDate();
+setInterval(updateDate, 10000);
+
+
