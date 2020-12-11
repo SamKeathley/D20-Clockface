06e259d0384c50245a870887cd34701bece8973d
Sam Keathley <samncollette@gmail.com>
2020-12-11 14:55:58 -0800

    fix date format from 00 at noon to 12

--- app/index.js
+++ app/index.js
@@ -1,55 +1,64 @@
-import clock from "clock";
-import document from "document";
-import { battery } from "power";
-import { preferences } from "user-settings";
-import * as util from "../common/utils";
+import clock from 'clock'
+import document from 'document'
+import { battery } from 'power'
+import { preferences } from 'user-settings'
+import * as util from '../common/utils'
 
-const myHour = document.getElementById("myHour");
-const myMin = document.getElementById("myMin");
-const myDate = document.getElementById("myDate");
-const myDayName = document.getElementById("myDayName");
-const myBattery = document.getElementById("myBattery");
+const myHour = document.getElementById('myHour')
+const myMin = document.getElementById('myMin')
+const myDate = document.getElementById('myDate')
+const myDayName = document.getElementById('myDayName')
+const myBattery = document.getElementById('myBattery')
 
-let time = document.getElementById("time")
-
-clock.granularity = "minutes";
+clock.granularity = 'minutes'
 clock.ontick = (evt) => {
-  let today = evt.date;
-  let hours = today.getHours();
-  if (preferences.clockDisplay === "12h") {
+  let today = evt.date
+  let hours = today.getHours()
+  if (preferences.clockDisplay === '12h') {
     // 12h format
-    hours = ("0" + hours % 12 || 12).slice(-2);
+    hours = hours % 12
+    hours = util.zeroHour(hours ? hours : 12)
   } else {
     // 24h format
-    hours = util.zeroPad(hours);
+    hours = util.zeroPad(hours)
   }
-  let mins = util.zeroPad(today.getMinutes());
-  myHour.text = `${hours}`;
-  myMin.text = `${mins}`;
+  let mins = util.zeroPad(today.getMinutes())
+  myHour.text = `${hours}`
+  myMin.text = `${mins}`
 }
 
 function updateBattery() {
-  let batteryLife = Math.floor(battery.chargeLevel) + "%";
-  myBattery.text = `${batteryLife}`;
+  let batteryLife = Math.floor(battery.chargeLevel) + '%'
+  myBattery.text = `${batteryLife}`
 }
 
-updateBattery();
-setInterval(updateBattery, 10000);
-
+updateBattery()
+setInterval(updateBattery, 10000)
 
 function updateDate() {
-  let months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
-  let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  let months = [
+    'JAN',
+    'FEB',
+    'MAR',
+    'APR',
+    'MAY',
+    'JUN',
+    'JUL',
+    'AUG',
+    'SEP',
+    'OCT',
+    'NOV',
+    'DEC'
+  ]
+  let days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
 
-  let today = new Date();
-  let month = months[today.getMonth()];
-  let dayName = days[today.getDay()];
-  let date = ("0" + today.getDate()).slice(-2);
-  myDate.text = `${date} ${month}`;
-  myDayName.text = `${dayName}`;
+  let today = new Date()
+  let month = months[today.getMonth()]
+  let dayName = days[today.getDay()]
+  let date = ('0' + today.getDate()).slice(-2)
+  myDate.text = `${date} ${month}`
+  myDayName.text = `${dayName}`
 }
 
-updateDate();
-setInterval(updateDate, 10000);
-
-
+updateDate()
+setInterval(updateDate, 10000)
--- common/utils.js
+++ common/utils.js
@@ -1,7 +1,15 @@
 // Add zero in front of numbers < 10
 export function zeroPad(i) {
   if (i < 10) {
-    i = "0" + i;
+    i = '0' + i
   }
-  return i;
+  return i
+}
+
+// Add zero in front of numbers < 12 for the hour
+export function zeroHour(j) {
+  if (j < 12) {
+    j = '0' + j
+  }
+  return j
 }
